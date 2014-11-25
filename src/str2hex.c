889aaae4d3c8f397ad8cacee83fff46066eb78b9
MAEKAWA Masahide <cvsync.mm@gmail.com>
2014-11-25 12:47:15 +0900

    Skip ':' as a separator.

--- src/str2hex.c
+++ src/str2hex.c
@@ -44,7 +44,7 @@ main(void)
 		ch = fgetc(stdin);
 		if ((ch == '\n') || (ch == EOF))
 			break;
-		if (isspace(ch))
+		if (isspace(ch) || (ch == ':'))
 			continue;
 		if (!isxdigit(ch))
 			exit(EXIT_FAILURE);
